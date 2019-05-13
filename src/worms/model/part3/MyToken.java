9fb88c77b31c7b245f5e83d704a79d909b612832
Hajagos Károly <hajagoskaroly.90@gmail.com>
2019-05-13 18:32:10 +0200

    Add files via upload
    interface implementacio

---
+++ src/worms/model/part3/MyToken.java
@@ -0,0 +1,69 @@
+package worms.model.part3;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenSource;
+
+public class MyToken implements Token {
+
+	@Override
+	public int getChannel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCharPositionInLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CharStream getInputStream() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getStartIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getStopIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTokenIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TokenSource getTokenSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
